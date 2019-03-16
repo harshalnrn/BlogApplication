d03dd2f79591cfac5ff74fbc7716f6001f5379cb
harshalnrn <harshalnrn@gmail.com>
2019-03-16 03:31:44 -0700

    Blog application: added homepage, login, logout functionalities

--- src/main/java/technicalblog/Model/NewUser.java
+++ src/main/java/technicalblog/Model/NewUser.java
@@ -1,4 +1,25 @@
 package technicalblog.Model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class NewUser {
+    private String fullName;
+    @Autowired
+    private User user;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/technicalblog/Service/UserRegService.java
+++ src/main/java/technicalblog/Service/UserRegService.java
@@ -1,13 +1,21 @@
 package technicalblog.Service;
 
+import org.springframework.stereotype.Service;
+import technicalblog.Model.NewUser;
 import technicalblog.Model.User;
 
+@Service
 public class UserRegService {
 
-    public void registerNewUser(User user){
+    public void registerNewUser(NewUser newUser){
 
-
+        System.out.println("new user registration details are as follows:");
+System.out.println("full name"+newUser.getFullName());
+       // System.out.println("username"+newUser.getUser().getUsername());  we shall get NPE here since user object is null
+       // System.out.println("password"+newUser.getUser().getPassword());
 
 
     }
 }
+
+//note if class is not annotated as service, its object wont be available in container
